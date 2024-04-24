568238c964403df648d3ff563610f8aa55660b1d
Risker <43505309+riskerboy@users.noreply.github.com>
2024-04-25 01:12:29 +0500

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    `maven-publish`
+}
+
+group = "dev.openrune"
+version = "1.2.4"
+
+repositories {
+    mavenCentral()
+    maven("https://raw.githubusercontent.com/OpenRune/hosting/master")
+}
+
+dependencies {
+
+    // https://mvnrepository.com/artifact/me.tongfei/progressbar
+    implementation("me.tongfei:progressbar:0.9.2")
+
+    // https://mvnrepository.com/artifact/com.displee/rs-cache-library
+    implementation("com.displee:rs-cache-library:7.1.0")
+// https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.1.0-alpha1")
+// https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+// https://mvnrepository.com/artifact/net.lingala.zip4j/zip4j
+    implementation("net.lingala.zip4j:zip4j:2.11.5")
+    implementation("dev.openrune:js5server:1.0.2")
+// https://mvnrepository.com/artifact/commons-io/commons-io
+    implementation("commons-io:commons-io:2.15.1")
+
+    implementation("com.github.jponge:lzma-java:1.3")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("it.unimi.dsi:fastutil:8.5.13")
+
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    languageVersion = "1.9"
+    jvmTarget = "11"
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("$buildDir/repo")
+        }
+        if (System.getenv("REPO_URL") != null) {
+            maven {
+                url = uri(System.getenv("REPO_URL"))
+                credentials {
+                    username = System.getenv("REPO_USERNAME")
+                    password = System.getenv("REPO_PASSWORD")
+                }
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
---
+++ src/main/kotlin/dev/openrune/cache/CacheManager.kt
@@ -0,0 +1,59 @@
+package dev.openrune.cache
+
+import dev.openrune.cache.filestore.Cache
+import dev.openrune.cache.filestore.definition.data.*
+import dev.openrune.cache.filestore.definition.decoder.*
+import java.nio.file.Path
+
+object CacheManager {
+
+
+    lateinit var cache: Cache
+    private lateinit var npcs: Array<NPCDefinition>
+    private lateinit var objects: Array<ObjectDefinition>
+    private lateinit var items: Array<ItemDefinition>
+    private lateinit var varbit: Array<VarBitDefinition>
+    private lateinit var varps: Array<VarpDefinition>
+    private lateinit var anim: Array<AnimDefinition>
+    private lateinit var enum: Array<EnumDefinition>
+    private var cacheRevision = -1
+
+
+    fun init(cachePath: Path, cacheRevision : Int) {
+        this.cacheRevision = cacheRevision;
+        cache = Cache.load(cachePath, false)
+        npcs = NPCDecoder().load(cache)
+        objects = ObjectDecoder().load(cache)
+        items = ItemDecoder().load(cache)
+        varbit = VarBitDecoder().load(cache)
+        varps = VarDecoder().load(cache)
+        anim = AnimDecoder().load(cache)
+        enum = EnumDecoder().load(cache)
+    }
+
+    fun getNpcs() = npcs
+    fun getObjects() = objects
+    fun getItems() = items
+
+    fun npc(id: Int): NPCDefinition = npcs[id]
+    fun npcCount(): Int = npcs.size
+
+    fun objects(id: Int): ObjectDefinition = objects[id]
+    fun objectCount(): Int = objects.size
+
+    fun item(id: Int): ItemDefinition = items[id]
+    fun itemCount(): Int = items.size
+
+    fun varbit(id: Int): VarBitDefinition = varbit[id]
+    fun varbitCount(): Int = varbit.size
+
+    fun varp(id: Int): VarpDefinition = varps[id]
+    fun varpCount(): Int = varps.size
+
+    fun anim(id: Int): AnimDefinition = anim[id]
+
+    fun enum(id: Int): EnumDefinition = enum[id]
+
+    fun revisionIsOrAfter(rev : Int) = rev <= cacheRevision
+    fun revisionIsOrBefore(rev : Int) = rev >= cacheRevision
+}
